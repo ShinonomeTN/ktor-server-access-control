e435fc98a7193dffc97bf8c354de27469bd7dd6e
CattenLinger <cattenlinger@outlook.com>
2022-05-17 02:11:54 +0800

    Add AccessControlRequirement and dsl

--- src/main/kotlin/com/shinonometn/ktor/server/access/control/DSL.kt
+++ src/main/kotlin/com/shinonometn/ktor/server/access/control/DSL.kt
@@ -31,3 +31,25 @@ fun Route.accessControl(checker: suspend AccessControlCheckerContext.() -> Unit,
     authorizationRoute.builder()
     return authorizationRoute
 }
+
+/**
+ * Add access control route.
+ * Use the given [requirement] to decides what data should be provided and what requests to child routes will be accepted.
+ * Default behavior is rejecting all request. To allow a request, the `accept()` in checker context must be call.
+ */
+@ContextDsl
+fun Route.accessControl(requirement : AccessControlRequirement, builder: Route.() -> Unit): Route {
+    val authorizationRoute = createChild(AccessControlRouteSelector())
+    application.feature(AccessControl).interceptPipeline(requirement.providerNames, authorizationRoute, requirement.checker)
+    authorizationRoute.builder()
+    return authorizationRoute
+}
+
+class AccessControlRequirement(vararg providerNames : String, val checker : AccessControlChecker) {
+    val providerNames = providerNames.toSet()
+    companion object {
+        fun default(checker : AccessControlChecker) : AccessControlRequirement {
+            return AccessControlRequirement(checker = checker)
+        }
+    }
+}
