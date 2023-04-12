f7f9699aa4909b4f080fe229c696b2e480825bb8
cattenlinger <lingerkong03@gmail.com>
2023-04-13 00:30:58 +0800

    Move methods

--- src/main/kotlin/com/shinonometn/ktor/server/access/control/AccessControlMetaSnapshot.kt
+++ src/main/kotlin/com/shinonometn/ktor/server/access/control/AccessControlMetaSnapshot.kt
@@ -7,3 +7,13 @@ interface AccessControlMetaSnapshot {
     /** The context's meta bucket. Storing simple values */
     val meta: Collection<Any>
 }
+
+/**
+ * Find metas with given type
+ */
+inline fun <reified T> AccessControlMetaSnapshot.metas() = meta.filterIsInstance<T>()
+
+/**
+ * Find first match meta with given type
+ */
+inline fun <reified T> AccessControlMetaSnapshot.meta() = meta.filterIsInstance<T>().firstOrNull()
