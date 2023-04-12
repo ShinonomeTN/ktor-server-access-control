8ab36613b740297aaee6399453ad372e8bceda1f
cattenlinger <lingerkong03@gmail.com>
2023-04-13 00:52:54 +0800

    Update tests

--- src/test/kotlin/com/shinonometn/ktor/server/access/control/AccessControlFeatureInstallTest.kt
+++ src/test/kotlin/com/shinonometn/ktor/server/access/control/AccessControlFeatureInstallTest.kt
@@ -6,17 +6,20 @@ import io.ktor.response.*
 import io.ktor.server.testing.*
 import org.junit.Test
 
+/**
+ * Test case : Feature installation
+ */
 class AccessControlFeatureInstallTest {
+
+    private val app : Application.() -> Unit = {
+        install(AccessControl) {
+            addMetaProvider {  }
+            doAfterReject { _, _ -> call.respond(HttpStatusCode.Forbidden) }
+        }
+    }
+
     @Test
     fun `Test install`() {
-        withTestApplication(Application::moduleTestAppFeatureInstall) {  }
-    }
-}
-
-/** The application for feature install testing */
-internal fun Application.moduleTestAppFeatureInstall() {
-    install(AccessControl) {
-        addMetaProvider {  }
-        doAfterReject { _, _ -> call.respond(HttpStatusCode.Forbidden) }
+        withTestApplication(app) {  }
     }
 }
