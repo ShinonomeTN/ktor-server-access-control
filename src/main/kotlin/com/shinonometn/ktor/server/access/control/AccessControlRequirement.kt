2b0e16bbad14b3a2af2b50609a785e4e15b23d99
cattenlinger <lingerkong03@gmail.com>
2023-04-13 00:39:35 +0800

    Deprecate the AccessControlRequirement builder.

--- src/main/kotlin/com/shinonometn/ktor/server/access/control/AccessControlRequirement.kt
+++ src/main/kotlin/com/shinonometn/ktor/server/access/control/AccessControlRequirement.kt
@@ -1,10 +1,24 @@
 package com.shinonometn.ktor.server.access.control
 
 interface AccessControlRequirement {
+    /**
+     * Meta providers required by this predicate
+     */
     val providerNames: Set<String>
+
+    /**
+    * Access control checker logics
+    */
     val checkers: List<AccessControlChecker>
 
     companion object {
+        /** Create an access control requirement */
+        operator fun invoke(vararg providerName: String, checker: AccessControlChecker) = object : AccessControlRequirement {
+            override val providerNames: Set<String> = providerName.toSet()
+            override val checkers: List<AccessControlChecker> = listOf(checker)
+        }
+
+        @Deprecated("Builder deprecated")
         class Builder internal constructor(
             private val providers: MutableList<String> = mutableListOf(),
             private val checkers: MutableList<AccessControlChecker> = mutableListOf()
@@ -18,6 +32,7 @@ interface AccessControlRequirement {
             fun build(): AccessControlRequirement = AccessControlRequirementImpl(providers.toSet(), checkers.toList())
         }
 
+        @Deprecated("Builder deprecated", ReplaceWith("AccessControlRequirement()") ,level = DeprecationLevel.WARNING)
         fun builder() = Builder()
     }
 }
